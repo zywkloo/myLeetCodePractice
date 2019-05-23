0344f2d237336e2b557d623e8c48183c60ab4ee6
Yiwei Zhang <zyw870315@163.com>
2019-05-23 00:59:41 -0400

    Create Easy_771.js

---
+++ Easy_771.js
@@ -0,0 +1,38 @@
+/**
+771. Jewels and Stones
+Easy
+You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+
+The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+Example 1:
+
+Input: J = "aA", S = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: J = "z", S = "ZZ"
+Output: 0
+Note:
+
+S and J will consist of letters and have length at most 50.
+The characters in J are distinct.
+
+
+ * @param {string} J
+ * @param {string} S
+ * @return {number}
+ */
+var numJewelsInStones = function(J, S) {
+    let set = new Set()
+    let count = 0
+    for (let jew of J){
+        set.add(jew)
+    }
+    for (let stone of S ){
+        if (set.has(stone)){
+            count++
+        }
+    }
+    return count
+};
